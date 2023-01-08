ef0a349227cf4603fb8295c6f5a28b510767ed60
Kiril Petriv <kiril.petriv@gymondo.de>
2023-01-08 11:53:38 +0100

    Added network module

---
+++ network/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "com.example.handson"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.material)
+    testImplementation(libs.test.junit4)
+    androidTestImplementation(libs.test.android.junit.ktx)
+    androidTestImplementation(libs.test.android.espresso)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "HandsOn"
 include (":app")
+include(":network")
